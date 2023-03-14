522ab1174f4f3ac1b44a8ffeaacec51552496a03
NishantLokhand <111300002+NishantLokhand@users.noreply.github.com>
2023-03-14 19:53:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-
+let complexArray = [
+  [
+    {
+      one: 1,
+      two: 2
+    },
+    {
+      three: 3,
+      four: 4
+    },
+	  {
+      five: 5,
+      six: 6
+    }
+  ],
+  [
+    {
+      a: "a",
+      b: "b"
+    },
+    {
+      c: "c",
+      d: "d"
+    },
+	  {
+      e: "e",
+      f: "f"
+    }
+  ]
+];
